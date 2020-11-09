1c6f0bb78201524b1eb3f640de5804560148dbbe
Aakash H Patel <ahprh12@gmail.com>
2020-11-09 09:37:23 -0800

    git ignore

--- NFLScores/nflsDBconnect.py
+++ NFLScores/nflsDBconnect.py
@@ -5,11 +5,14 @@
 
 from pymongo import MongoClient
 
+# prod
+#mongo_uri = "mongodb+srv://nfls_user:kawboy@cluster0.3kay6.mongodb.net/nfls?retryWrites=true&w=majority"
+
 # local db testing
 #mongo_uri = "mongodb://nfls_user:kawboy@localhost:27017/nfls"
 
 try:
-    mongo_uri = "mongodb+srv://nfls_user:kawboy@cluster0.3kay6.mongodb.net/nfls?retryWrites=true&w=majority"
+    mongo_uri = "mongodb://nfls_user:kawboy@localhost:27017/nfls"
 except KeyError:
     from secrets import mongoURI
     mongo_uri = mongoURI
